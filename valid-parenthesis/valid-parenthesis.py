67fd99b4770fb90935333dc173e0767484dd6255
am401 <andras.marton@wpengine.com>
2021-02-25 23:34:57 -0600

    Solved script

--- valid-parenthesis/valid-parenthesis.py
+++ valid-parenthesis/valid-parenthesis.py
@@ -6,8 +6,18 @@ class Solution:
         for i in s:
             if i in open_brackets:
                 stack.append(i)
+            elif i in closed_brackets and not stack:
+                return False
             elif i in closed_brackets:
-                stack = stack[0:-1]
+                top = stack[-1]
+                if i == ')' and top == '(':
+                    stack = stack[0:-1]
+                elif i == '}' and top == '{':
+                    stack = stack[0:-1]
+                elif i == ']' and top == '[':
+                    stack = stack[0:-1]
+                else:
+                    return False
         if len(stack) == 0:
             return True
         else:
